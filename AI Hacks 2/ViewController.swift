21d3ce371499535cd9d27c8c845f9db65eaa3f33
Vanessa2363 <43653210+Vanessa2363@users.noreply.github.com>
2020-09-06 00:57:55 -0700

    Commit 1

--- AI Hacks 2/ViewController.swift
+++ AI Hacks 2/ViewController.swift
@@ -8,13 +8,107 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+class Cell: UITableViewCell {
+    
 }
 
+class ViewController: UIViewController {
+
+    @IBOutlet weak var ageButton: UIButton!
+    @IBOutlet weak var experienceButton: UIButton!
+    @IBOutlet weak var payButton: UIButton!
+    @IBOutlet weak var subjectButton: UIButton!
+    
+    let transparentLayer = UIView()
+    let tableView = UITableView()
+    
+    var buttonClicked = UIButton()
+    
+    var Data = [String]()
+    
+    override func viewDidLoad() {
+        tableView.register(Cell.self , forCellReuseIdentifier: "classCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        super.viewDidLoad()
+    }
+    
+    func addTransparentLayer(frames:CGRect) {
+        let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+        transparentLayer.frame = window?.frame ?? self.view.frame
+        self.view.addSubview(transparentLayer)
+        
+        tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height, width: frames.width, height: 0)
+        self.view.addSubview(tableView)
+        tableView.layer.cornerRadius = 3
+        self.tableView.isScrollEnabled = true
+        
+//Adds transparent layer when a button is clicked
+        transparentLayer.backgroundColor = UIColor.blue.withAlphaComponent(0.6)
+//Allows the tableView to change every time instead of keeping the same Data
+        tableView.reloadData()
+        
+        let clickMotion = UITapGestureRecognizer(target: self, action: #selector(removeTransparentLayer))
+        transparentLayer.addGestureRecognizer(clickMotion)
+// Places transparency without animation
+        transparentLayer.alpha = 0
+        UIView.animate(withDuration: 0.3, delay: 0.0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+            self.transparentLayer.alpha = 0.4
+//height determines how many boxes/options will show up due to space
+            self.tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height + 7, width: frames.width, height: CGFloat(self.Data.count * 40))
+        }, completion: nil)
+    }
+    
+    @objc func removeTransparentLayer() {
+        let frames = buttonClicked.frame
+        UIView.animate(withDuration: 0.3, delay: 0.0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.9, options: .curveEaseInOut, animations: {
+            self.transparentLayer.alpha = 0
+            self.tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height, width: frames.width, height: 0)
+        }, completion: nil)
+    }
+    
+    @IBAction func clickAgeButton(_ sender: Any) {
+        Data = ["8-12", "13-15", "16-18", "18-25", "25+" ]
+// Can't get rid of spring
+//        Data = ["8","9","10","11","12","13","14","15","16","17","18", "19". "20", "21", "22", "23", "24", "25",]
+        buttonClicked = ageButton
+        addTransparentLayer(frames: ageButton.frame)
+    }
+    @IBAction func clickExperienceButton(_ sender: Any) {
+        Data = ["Novice", "Intermediate", "Advanced", "Expert"]
+        buttonClicked = experienceButton
+        addTransparentLayer(frames: experienceButton.frame)
+    }
+    @IBAction func clickPayButton(_ sender: Any) {
+        Data = ["Paying", "Unpaying"]
+        buttonClicked = payButton
+        addTransparentLayer(frames: payButton.frame)
+    }
+    @IBAction func clickSubjectButton(_ sender: Any) {
+        Data = ["Science", "Technology", "Engineering", "Math"]
+        buttonClicked = subjectButton
+        addTransparentLayer(frames: subjectButton.frame)
+    }
+    
+    }
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "classCell", for: indexPath)
+        cell.textLabel?.text = Data[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        buttonClicked.setTitle(Data[indexPath.row], for: .normal)
+        removeTransparentLayer()
+    }
+}

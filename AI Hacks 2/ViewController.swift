135dafd935d1d63471f87a0fa4651355d7e79e44
Vanessa2363 <43653210+Vanessa2363@users.noreply.github.com>
2020-09-05 20:41:30 -0700

    Initial Commit

---
+++ AI Hacks 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AI Hacks 2
+//
+//  Created by Vanessa Guo on 9/5/20.
+//  Copyright © 2020 Vanessa Guo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
